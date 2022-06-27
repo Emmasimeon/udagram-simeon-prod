8ccfa00a52b8636d48c98b0319201c002f524364
EmmaSimeon <Emmasimeon3@gmail.com>
2022-06-27 19:50:08 +0100

    1st_build

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+export const config = {
+    "prod": {
+      "username": "udagramsimeonprod",
+      "password": "udagramsimeonprod",
+      "database": "udagram_prod",
+      "host": "udagramsimeonprod.cwjchwqdgdfh.us-east-1.rds.amazonaws.com",
+      "dialect": "postgres",
+      "aws_region": "us-east-1",
+      "aws_profile": "default",
+      "aws_media_bucket": "udagramsimeonprod"
+    },
+    "jwt": {
+      "secret": " "
+    }
+  }
+  
